ee0921be62fd84323ec721ed361c30784a55daca
Ammar Salahi <ammarsaalahi@gmail.com>
2025-08-24 23:42:21 +0330

    add delete modal

--- src/components/global/DeleteModal.tsx
+++ src/components/global/DeleteModal.tsx
@@ -4,16 +4,20 @@ import { Api } from '../api/Index';
 import { CATEGORIES, CATEGORIES_ID, SOCIALS } from '../api/Endpoints';
 import { message } from 'antd';
 import { useRecoilValue } from 'recoil';
-import { categorySelector, tokenSelector } from '../states/Selectors';
+import { categorySelector, langSelector, tokenSelector } from '../states/Selectors';
 import { useTranslation } from 'react-i18next';
 
 interface modalProps{
-  id:number;  
-  close:any;
+  close:()=>void;
+  delete:()=>void;
+  type:string;
+  name:string
+  engname:string
+
 }
 export default function DeleteModal(props:modalProps) {
   const token=useRecoilValue(tokenSelector)
-
+  const lang = useRecoilValue(langSelector)
   const {t} = useTranslation();
 
   const handleClose=()=>{
@@ -28,21 +32,26 @@ export default function DeleteModal(props:modalProps) {
     <dialog id={"delmodal"} className="modal">
         <div className="modal-box py-3">
           <div className="flex justify-between ps-5 items-center">
-             <h3 className="font-bold text-lg text-center">حذف</h3>
+             <h3 className="font-bold text-lg text-center">{t('delete')}</h3>
               <button className='btn btn-circle btn-ghost text-2xl' onClick={handleClose}>
                 <CgClose/>
               </button>
           </div>
             <div className='py-4 md:py-10 md:px-5 space-y-5  p-0'>
-               
+               {lang == "fa"?
+                    <p className='text-base' dir='lt'>{`آیا میخواهید ${t(`${props.type}`)} " ${props.name} "  را حذف کنید؟!`}</p>
+                :
+                    <p className='text-base' dir='lt'>{`Do you want to delete ${t(`${props.type}`)} "${props.engname}" ?! `}</p>
+               }
               <div className="flex justify-between items-center  gap-3" dir="rtl">
                 <button 
                     className='btn-red w-[50%] md:w-52 rounded-2xl' type='button' 
+                    onClick={props.delete}
                 >
-                  {t('agree')}
+                  {t('yes')}
                 </button>
                 <button className='btn  w-[50%] md:w-52 rounded-2xl' type='button' onClick={handleClose}>
-                  {t('cancel')}
+                  {t('no')}
                 </button>
               </div>
             </div>
