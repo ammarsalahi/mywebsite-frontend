f0ac362c7f869d80fbd642cb0b9e7513d025d6ba
Ammar Salahi <ammarsaalahi@gmail.com>
2025-08-27 22:23:43 +0330

    fix footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,37 +1,59 @@
-import React,{useEffect, useState} from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { FaTelegram,FaInstagram ,FaLinkedin,FaGithub} from "react-icons/fa";
+
+import React, { useEffect, useState } from "react";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import logoblack from "../../assets/logo-dark.png";
+import logolight from "../../assets/logo-light.png";
+import { PiNewspaperFill } from "react-icons/pi";
+import { FaGithub, FaHammer, FaHandshake, FaTelegram } from "react-icons/fa6";
+import { BsEmojiSunglassesFill } from "react-icons/bs";
+import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Api } from "../api/Index";
+import { ABOUTS_ID, FOOTER } from "../api/Endpoints";
+import { useRecoilValue } from "recoil";
+import { themeSelector, tokenSelector } from "../states/Selectors";
 import { MdEmail } from "react-icons/md";
-import { Api } from '../api/Index';
-import { ABOUTS_ID, FOOTER } from '../api/Endpoints';
-import { TbWorldWww } from 'react-icons/tb';
-import { useRecoilValue } from 'recoil'
-import { themeSelector, tokenSelector } from '../states/Selectors';
-import { SiGmail } from 'react-icons/si';
-import logoWhite from '../../assets/icon-light.png'
-import logoBlue from '../../assets/icon-blue2.png'
-import {motion} from 'framer-motion'
+import { SiGmail } from "react-icons/si";
+import { TbWorldWww } from "react-icons/tb";
 
-
-export default function Footer() {
-  const location=useLocation()
-  const [categories,setCategories]=useState<any>([])
-  const [teches,setTeches]=useState<any>([]);
-  const [socials,setSocials]=useState<any>([]);
-  const theme=useRecoilValue(themeSelector)
-  const token=useRecoilValue(tokenSelector)
-   const getData=async()=>{
-	 await Api.get(FOOTER).then((res)=>{
+const Footer = () => {
+    const { t } = useTranslation();
+	const location=useLocation()
+	const [categories,setCategories]=useState<any>([]);
+	const [socials,setSocials]=useState<any>([]);
+	const theme=useRecoilValue(themeSelector)
+  	const token=useRecoilValue(tokenSelector)
+	const menusFa = [
+		{
+		  url: "/posts",
+		  icon: <PiNewspaperFill fontSize={22} />,
+		},
+		{
+		  url: "/projects",
+		  icon: <FaHammer fontSize={22} />,
+		},
+		{
+		  url: "/about",
+		  icon: <BsEmojiSunglassesFill fontSize={22} />,
+		},
+		{
+		  url: "/collaboration",
+		  icon: <FaHandshake fontSize={22} />,
+		},
+    ];
+   
+	const getData=async()=>{
+	 	await Api.get(FOOTER).then((res)=>{
 		setCategories(res.data.categories);
-		setTeches(res.data.teches)
-	 });
+	 	});
 
-   }
-   const getAbout=async()=>{
-    await Api.get(ABOUTS_ID(token.user)).then((res)=>{
-      setSocials(res.data.socials)
-    })
-  }
+   	}
+
+	const getAbout=async()=>{
+		await Api.get(ABOUTS_ID(token.user)).then((res)=>{
+		  setSocials(res.data.socials)
+		})
+	}
   const getIcon=(name:string)=>{
 	let result:any;
 	switch(name){
@@ -60,61 +82,82 @@ export default function Footer() {
 
 	return result
    }
-   useEffect(()=>{
-     getData();
-	if(location.pathname!=='/about'){
-	 	getAbout()
-	}
-   },[location.pathname])
+
+	useEffect(()=>{
+		getData()
+		getAbout()
+	},[])
   return (
-    <div className={
-      theme=="light"?
-      location.pathname=='/'?'bg-blue-600  w-full text-white':'border-t w-full'
-      :
-       'bg-gray-900  w-full text-white border-t border-gray-700'}>
-      <div className="py-3">
-      	<div className="footer-category">
-		  <div className="footer-cols flex justify-center">
-            <div className="relative px-20 py-4">
-				<div>
-					{/* <motion.img
-						src={theme=="light"&&location.pathname!=="/"?logoBlue:logoWhite}
-						className="h-32 mb-5"
-						initial={{ scale: 1 }}
-						animate={{ rotate: 360 }}
-						transition={{
-							type: "spring",
-							stiffness: 110,
-							damping: 40,
-							repeat:Infinity,
-							repeatType:"reverse"
-						}}
-					/> */}
-					<img src={theme=="light"&&location.pathname!=="/"?logoBlue:logoWhite} className='h-36 mb-5'/>
-				</div>
-				<div className=' text-center'>
-					<span className='text-base font-semibold'>&copy; 2024</span>
-				</div>
-			</div>
-		  </div>
-      	  <div className="footer-cols lg:col-span-2 lg:px-5 py-3">
-			{categories.length>0&&
-			<>
-      	     <div className="p-2 pt-10">
-      	     <ul className="footer-list text-center">
-				{categories?.map((item:any,idx:number)=>(
-					<li className="text-lg font-semibold py-3" key={idx}>
+    <footer className={`${theme=="light"?
+      	location.pathname=='/'?'bg-blue-600  w-full text-white':'border-t w-full'
+      	:
+       'bg-gray-900  w-full text-white border-t border-gray-700'} pt-12 pb-20 md:pb-3 px-6`}
+	   >
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8" dir={t("dir")}>
+        {/* Logo & Description */}
+        <div>
+          <h2 className="text-2xl font-bold text-blue-400 mb-4 text-center">
+			{location.pathname=="/"?
+			<img src={logolight} alt="" className="w-[200px]" />
+			:
+			<img src={theme=="dark"?logolight:logoblack} alt="" className="w-[200px]" />
+			}
+		  </h2>
+          <p className="text-sm leading-relaxed">
+            یتنسیتبسینیب
+          </p>
+        </div>
+
+        {/* Links */}
+        <div>
+          <h3 className="text-lg font-semibold mb-4">صفحات</h3>
+          <ul className="space-y-4 text-sm">
+			{menusFa.map((item: any, idx: number) => (
+					  <li className={`${location.pathname=="/"?(theme=="dark"?"hover:text-blue-600":""):"hover:text-blue-600"}`} key={idx} dir={t("dir")}>
+						<Link to={item.url} className="flex items-center gap-1">
+						  {item.icon}
+						  {t(`menu${idx + 1}`)}
+						</Link>
+					  </li>
+					))}
+          </ul>
+        </div>
+
+        {/* Others */}
+        {categories.length>0?
+		<div>
+         
+		  <h3 className="text-lg font-semibold mb-4">دسته‌بندی‌ها</h3>
+          	
+			<ul className="space-y-2 text-sm">
+				{categories?.slice(0,7).map((item:any,idx:number)=>(
+					<li className={`${location.pathname=="/"?(theme=="dark"?"hover:text-blue-600":""):"hover:text-blue-600"}`} key={idx}>
 						<Link to={`/categories/${item.english_name}`}>{item.name}</Link>
 					</li>
 				))}
-      	    
-      	     </ul>
-      	     </div>
-      	     </>}
-      	  </div>
-      	</div>
-	
+       	    
+          </ul>
+        </div>:
+		<div></div>}
+
+        {/* Social Media */}
+        {socials.length>0 && <div>
+          <h3 className="text-white font-semibold mb-4">شبکه های اجتماعی</h3>
+          <div className="flex justify-start gap-x-4 text-xl">
+			{socials.map((item:any,idx:number)=>(
+				<Link to={item.link} key={idx}>
+				  {getIcon(item.name)}
+				</Link>
+			))}
+
+          </div>
+        </div>}
       </div>
-    </div>
-  )
-}
+	  <div className="flex justify-center items-center pt-8">
+		<p>2025 &copy;</p>
+	  </div>
+    </footer>
+  );
+};
+
+export default Footer;
